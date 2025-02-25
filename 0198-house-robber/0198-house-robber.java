9274ed532cf6692d544cd01b8a040c9c5131e9d0
zeezxu <166929730+zeezxu@users.noreply.github.com>
2025-02-24 21:26:06 -0500

    Time: 0 ms (100%), Space: 41 MB (75.37%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -5,16 +5,19 @@ class Solution {
         if(N==0){
             return 0;
         }
-
-        int[] maxRob = new int[nums.length + 1];
-
-        maxRob[N] = 0;
-        maxRob[N-1] = nums[N-1];
-
-        for (int i = N-2; i >= 0; i--){
-            maxRob[i] = Math.max(maxRob[i+1], maxRob[i+2] + nums[i]);
+        if(N==1){
+            return nums[0];        
         }
 
-        return maxRob[0];
+        int[] maxRob = new int[N];
+
+        maxRob[0] = nums[0];
+        maxRob[1] = Math.max(nums[0], nums[1]);
+
+        for (int i = 2; i < N; i++){
+            maxRob[i] = Math.max(maxRob[i-1], maxRob[i-2] + nums[i]);
+        }
+
+        return maxRob[N-1];
     }
 }
