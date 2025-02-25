5f2266bd41ebb938f3f1818750714ef20f25ea8d
zeezxu <166929730+zeezxu@users.noreply.github.com>
2025-02-25 00:19:22 -0500

    Time: 1 ms (99.55%), Space: 45.1 MB (58.82%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        ArrayList<String> arr = new ArrayList<String>(); 
+
+        for (int i = 1; i <= n; i++){
+            if (i%3==0 && i%5 == 0){
+                arr.add("FizzBuzz");
+            }
+            else if (i%3 == 0 && i%5 != 0){
+                arr.add("Fizz");
+            }
+            else if (i%5 == 0 && i%3 != 0){
+                arr.add("Buzz");
+            }
+            else{
+                arr.add(String.valueOf(i));
+            }
+
+        }
+        return arr;
+
+    }
+}
