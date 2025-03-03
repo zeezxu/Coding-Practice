daf633164ad5c93997dd551dfc969606c8aa25e8
zeezxu <166929730+zeezxu@users.noreply.github.com>
2025-03-02 23:32:39 -0500

    Time: 30 ms (67.77%), Space: 51.5 MB (59.37%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -26,14 +26,16 @@ class Solution {
                 }
                 else{
                     res.add(Arrays.asList(nums[i], nums[left], nums[right]));
-                    left++;
-                    right--;
-                    while(left<right && nums[right] == nums[right+1]){
+                   
+                    while(left<right && nums[right] == nums[right-1]){
                         right--;
                     }
-                    while(left<right && nums[left] == nums[left-1]){
+                    while(left<right && nums[left] == nums[left+1]){
                         left++;
                     }
+
+                    left++;
+                    right--;
                     
                 }
             }
