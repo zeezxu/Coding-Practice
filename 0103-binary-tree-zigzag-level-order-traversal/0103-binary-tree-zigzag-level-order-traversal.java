b3af55f9727422d08093bc558e3c4ab6111423a9
zeezxu <166929730+zeezxu@users.noreply.github.com>
2025-03-22 23:47:22 -0400

    Time: 0 ms (100%), Space: 42.2 MB (58.66%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -15,39 +15,39 @@
  */
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        if(root == null){
+        if(root == null){       //空树
             return new ArrayList<List<Integer>>();
         }
 
-        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        List<List<Integer>> res = new ArrayList<List<Integer>>(); //最终返回列表
 
-        LinkedList<TreeNode> node_queue = new LinkedList<TreeNode>();
+        LinkedList<TreeNode> node_queue = new LinkedList<TreeNode>();  // 节队列，里面存放每一行的node
         node_queue.addLast(root);
-        node_queue.addLast(null);
+        node_queue.addLast(null);  //用null来标记这一行已结束
 
-        LinkedList<Integer> level_list = new LinkedList<>();
+        LinkedList<Integer> level_list = new LinkedList<>();  // level链表,用来储存单一行的节点
 
-        boolean is_from_left = true;
+        boolean is_from_left = true;  //是否是从左往右的顺序
 
-        while(node_queue.size()>0){
-            TreeNode curr_node = node_queue.pollFirst();
-            if(curr_node != null){
+        while(node_queue.size()>0){     //当node队列不为空时
+            TreeNode curr_node = node_queue.pollFirst();    //从队列中pollFirst
+            if(curr_node != null){    //当前node不为null时
                 if(is_from_left){
-                    level_list.addLast(curr_node.val);
+                    level_list.addLast(curr_node.val); //将当前node的值从后往前加入level链表
                 }
                 else{
-                    level_list.addFirst(curr_node.val);
+                    level_list.addFirst(curr_node.val); //将当前node的值从前往后加入level链表
                 }
                 if(curr_node.left != null){
-                    node_queue.addLast(curr_node.left);
+                    node_queue.addLast(curr_node.left);   //将当前node的左子节加入node队列，准备下一行
                 }
                 if(curr_node.right != null){
-                    node_queue.addLast(curr_node.right);
+                    node_queue.addLast(curr_node.right);  //将当前node的右子节加入node队列，准备下一行
                 }
             }
             else{
-                res.add(level_list);
-                level_list = new LinkedList<Integer>();
+                res.add(level_list);  //如果遇到null node,则当前行以结束，res加入当前level
+                level_list = new LinkedList<Integer>(); 
 
                 if(node_queue.size() > 0){
                     node_queue.addLast(null);
